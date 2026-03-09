f8a3bc81ec9da4287edca76f9e707624f445adad
vishalsinha2004 <vishalsinha6567@gmail.com>
2026-03-10 01:29:30 +0530

    update url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'http://127.0.0.1:8000/api/',
     baseURL: 'https://parceel.onrender.com',
 });
 
